6930375205101047df0c2f930651546c63a9b4af
dotBlueShoes <matthewstrumillo@gmail.com>
2023-10-12 13:37:31 +0200

    armor & tools stats

--- src/main/java/dotBlueShoes/GemsMod/materials/ArmorMaterials.java
+++ src/main/java/dotBlueShoes/GemsMod/materials/ArmorMaterials.java
@@ -7,55 +7,99 @@ import turniplabs.halplibe.helper.ArmorHelper;
 
 public class ArmorMaterials {
 
-	public static final int DEF_DURABILITY = 800;   // +50
-	public static final float DEF_COMBAT = 66.0f;   // +10
-	public static final float DEF_BLAST = 66.0f;    // +10
-	public static final float DEF_FIRE = 124.0f;    // +10
-	public static final float DEF_FALL = 66.0f;     // +10
+	// DEFAULTS - DIAMOND
+	static final int DEF_DURABILITY = 800;
+	static final float DEF_COMBAT = 66.0f;
+	static final float DEF_BLAST = 66.0f;
+	static final float DEF_FIRE = 124.0f;
+	static final float DEF_FALL = 66.0f;
+
+	// STEPS - I am lazy :)
+	static final int    S1 = 50;
+	static final float  S2 = 9;
+	static final float  S3 = 9;
+	static final float  S4 = 23;
+	static final float  S5 = 9;
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_TOPAZ = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_TOPAZ),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY - (2 * S1),
+		DEF_COMBAT + (2 * S2),
+		DEF_BLAST + (2 * S3),
+		DEF_FIRE - (4 * S4),
+		DEF_FALL + (1 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_TANZANITE = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_TANZANITE),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY + S1,
+		DEF_COMBAT + (2 * S2),
+		DEF_BLAST + (1 * S3),
+		DEF_FIRE - (1 * S4),
+		DEF_FALL - (2 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_SAPPHIRE = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_SAPPHIRE),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY - S1,
+		DEF_COMBAT - (2 * S2),
+		DEF_BLAST + (2 * S3),
+		DEF_FIRE + (2 * S4),
+		DEF_FALL - (2 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_RUBY = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_RUBY),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY - S1,
+		DEF_COMBAT + (1 * S2),
+		DEF_BLAST - (1 * S3),
+		DEF_FIRE - (2 * S4),
+		DEF_FALL + (2 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_PERIDOT = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_PERIDOT),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY + S1,
+		DEF_COMBAT - (3 * S2),
+		DEF_BLAST + (2 * S3),
+		DEF_FIRE - (3 * S4),
+		DEF_FALL + (2 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_MALACHITE = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_MALACHITE),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY + (2 * S1),
+		DEF_COMBAT - (1 * S2),
+		DEF_BLAST - (1 * S3),
+		DEF_FIRE - (2 * S4),
+		DEF_FALL - (1 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_EMERALD = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_EMERALD),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY - (4 * S1),
+		DEF_COMBAT - (1 * S2),
+		DEF_BLAST - (2 * S3),
+		DEF_FIRE + (1 * S4),
+		DEF_FALL + (2 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_AMBER = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_AMBER),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY,
+		DEF_COMBAT + (1 * S2),
+		DEF_BLAST - (1 * S3),
+		DEF_FIRE + (1 * S4),
+		DEF_FALL - (1 * S5)
 	);
 
 	public static final ArmorMaterial ARMOR_MATERIAL_GEM_BLACK_DIAMOND = ArmorHelper.createArmorMaterial(
 		Registry.getModPath(Global.S_BLACK_DIAMOND),
-		800, 66.0f, 66.0f, 124.0f, 66.0f
+		DEF_DURABILITY * 2,
+		DEF_COMBAT + (6 * S2),
+		DEF_BLAST + (2 * S3),
+		DEF_FIRE + (2 * S4),
+		DEF_FALL + (6 * S5)
 	);
 
 	public static final ArmorMaterial[] ARMOR_MATERIALS = {
--- src/main/java/dotBlueShoes/GemsMod/materials/ToolMaterials.java
+++ src/main/java/dotBlueShoes/GemsMod/materials/ToolMaterials.java
@@ -4,76 +4,82 @@ import net.minecraft.core.item.material.ToolMaterial;
 
 public class ToolMaterials {
 
+	// DEFAULTS - DIAMOND
+	static final int DEF_DURABILITY         = 1536;
+	static final float DEF_EFFICIENCY       = 14.0f;
+	static final float DEF_HASTE_EFFICIENCY = 30.0f;
+	static final int DEF_DAMAGE           = 4;
+	static final int DEF_BLOCK_HIT_DELAY  = 4;
+
+	// STEPS - I am lazy :)
+	static final int    S1 = 192;
+	static final float  S2 = 4;
+	static final float  S3 = 6;
+	static final int    S4 = 1;
+
 	public static final ToolMaterial TOOL_MATERIAL_GEM_TOPAZ = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY - S1)
+		.setEfficiency(DEF_EFFICIENCY + S2, DEF_HASTE_EFFICIENCY - (2 * S3))
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
+		.setDamage(DEF_DAMAGE + S4)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY)
 		.setSilkTouch(true);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_TANZANITE = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY + S1)
+		.setEfficiency(DEF_EFFICIENCY - (2 * S2), DEF_HASTE_EFFICIENCY - (2 * S3))
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE + S4)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_SAPPHIRE = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY + S1)
+		.setEfficiency(DEF_EFFICIENCY - S2, DEF_HASTE_EFFICIENCY)
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE - S4)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_RUBY = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY - (2 * S1))
+		.setEfficiency(DEF_EFFICIENCY, DEF_HASTE_EFFICIENCY)
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE + S4)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_PERIDOT = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY + S1)
+		.setEfficiency(DEF_EFFICIENCY - (1 * S2), DEF_HASTE_EFFICIENCY + (1 * S3))
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_MALACHITE = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
-		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDurability(DEF_DURABILITY - (6 * S1))
+		.setEfficiency(DEF_EFFICIENCY - (3 * S2), DEF_HASTE_EFFICIENCY - (3 * S3))
+		.setMiningLevel(4)
+		.setDamage(DEF_DAMAGE - S4)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_EMERALD = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY - (2 * S1))
+		.setEfficiency(DEF_EFFICIENCY + (2 * S2), DEF_HASTE_EFFICIENCY + (2 * S3))
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE - (2 * S4))
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_AMBER = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY + (6 * S1))
+		.setEfficiency(DEF_EFFICIENCY + S2, DEF_HASTE_EFFICIENCY - (3 * S3))
 		.setMiningLevel(3)
-		.setDamage(4)
-		.setBlockHitDelay(4)
-		.setSilkTouch(true);
+		.setDamage(DEF_DAMAGE - (2 * S4))
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY);
 
 	public static final ToolMaterial TOOL_MATERIAL_GEM_BLACK_DIAMOND = new ToolMaterial()
-		.setDurability(512)
-		.setEfficiency(14.0f, 45.0f)
+		.setDurability(DEF_DURABILITY * 2)
+		.setEfficiency(DEF_EFFICIENCY, DEF_HASTE_EFFICIENCY)
 		.setMiningLevel(4)
-		.setDamage(4)
-		.setBlockHitDelay(4)
+		.setDamage(DEF_DAMAGE)
+		.setBlockHitDelay(DEF_BLOCK_HIT_DELAY)
 		.setSilkTouch(true);
 
 	public static final ToolMaterial[] TOOL_MATERIALS = {
